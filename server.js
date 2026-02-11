1e484e401ecaaaf946c2714c86083eb354fef039
Gavin Waako <gavin.sonny27@gmail.com>
2026-02-11 18:59:25 -0500

    Create users and new users endpoints

--- server.js
+++ server.js
@@ -9,4 +9,12 @@ app.get("/", (req, res) => {
   res.render("index", { text: "Hello, World!" });
 });
 
+app.get("/users", (req, res) => {
+  res.send("Users endpoint");
+});
+
+app.get("/users/new", (req, res) => {
+  res.send("New users form.");
+});
+
 app.listen(3000);
