f10503ddc9027c9249ffd020f79e3d5caa4a127c
Gavin Waako <gavin.sonny27@gmail.com>
2026-02-11 18:51:48 -0500

    Add home route

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   console.log("Hello world");
-  //   res.send("Hello world");
-  res.render("index.html");
+  res.render("index", { text: "Hello, World!" });
 });
 
 app.listen(3000);
